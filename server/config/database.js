56b5dc06ad0455e6332b5e1ad5025c0d81201bb5
catatankuliah-lab <nanasupriatnakursus@gmail.com>
2024-08-14 21:36:49 +0700

    create stunting 88 log

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('stuntingdelapandelapanlogistics', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize('stuntingdelapandelapanlogistics', 'itsstunting', 'lfDLstKMgZopfcTgWj5l', {
+  host: '195.35.7.27',
   dialect: 'mysql',
   dialectOptions: {
     connectTimeout: 60000

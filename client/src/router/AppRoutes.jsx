747323be742aead0e40eaed5803bef739ed2e7c0
catatankuliah-lab <nanasupriatnakursus@gmail.com>
2024-08-20 22:58:10 +0700

    create struktur tabel vendor dan armada by nana

--- client/src/router/AppRoutes.jsx
+++ client/src/router/AppRoutes.jsx
@@ -3,13 +3,15 @@ import { Route, Routes } from 'react-router-dom';
 import DashboardPage from '../pages/DashboardPage';
 import IndexWOPageITSKancab from '../pages/itsupportkancab/wo/indexPage';
 import IndexLOPageITSKancab from '../pages/itsupportkancab/lo/indexPage';
+import IndexvendorPageITSKancab from '../pages/itsupportkancab/vendor/indexPage';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<DashboardPage />} />
-            <Route path="/itsupportkancab/wo" element={<IndexWOPageITSKancab />} />
+            <Route path="/itsupportkancab/vendor" element={<IndexvendorPageITSKancab />} />
             <Route path="/itsupportkancab/lo" element={<IndexLOPageITSKancab />} />
+            <Route path="/itsupportkancab/wo" element={<IndexWOPageITSKancab />} />
         </Routes>
     );
 };
